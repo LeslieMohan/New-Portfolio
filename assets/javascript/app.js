04addac3288d1ca201eaeada062bd4409e6acd2a
LeslieMohan <donnalesliedesign@gmail.com>
2018-07-03 12:58:15 -0400

    updated with a lot of changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 $(window).scroll(function() {
 
 
-    var wscroll = $(this).scrollTop();
+    var wScroll = $(this).scrollTop();
     
-    console.log(wscroll);
+    console.log(wScroll);
     console.log(window.scrollY);
     //IF scrollY is at bottom of aBout Me section header then set the class on the imge to slide/fade in
     //display:none (image and paragraph)
@@ -17,18 +17,88 @@ $(window).scroll(function() {
     });*/
 
     $('#sq4').css({
-        'transform': 'translate(0px, '+ wscroll /1 +'%)'
+        'transform': 'translate(0px, '+ wScroll /1 +'%)'
     
     });
+    
     /*$('').css({
-        'transform': 'translate(0px, '+ wscroll /2 +'%)'
+        'transform': 'translate(0px, '+ wScroll /2 +'%)'
     
     });*/
 
     /*$('#bio-image').css({
-        'transform': 'translate(0px, '+ wscroll /2 +'%)'
+        'transform': 'translate(0px, '+ wScroll /2 +'%)'
     
     });*/
+    
+    if (wScroll <= 350) {
+        $('#about').hide();
+        $('#home').show();
+        $('#projects').hide();
+
+    }
+
+    else if (wScroll >= 351) {
+        setTimeout(function(){
+            $('#bio-image').fadeIn();
+        }), 300;
+        $('#about').show();
+        $('#home').hide();
+        $('#projects').hide();
+    
+    } else if (wScroll >= 1063) {
+        $('#bio-image').fadeOut();
+        $('#about').hide();
+        $('#home').hide();
+        $('#projects').show();
 
 
-});   
+    }
+
+  
+
+
+
+    /*if (wScroll > $('.sectTwo').offset().top-500) {
+            setTimeout(function(){
+                $('#bio-image').addClass('show');
+            }), 180
+            
+        };*/
+
+    if (wScroll > $('.main-carousel').offset().top-500) {
+            setTimeout(function(){
+                $('h4').addClass('show');
+            }), 180
+            
+        };
+
+
+    if (wScroll > $('.portContainer').offset().top-500) {
+        $('.portSq').each(function(i){ 
+                console.log('hello');
+            setTimeout(function(){
+                $('.portSq').eq(i).addClass('show');
+            }, 180 * (i+1));
+            
+        });
+    };
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); 
+  
